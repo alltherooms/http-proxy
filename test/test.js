5b7e2b2a07e2416bbc4d9a19b8ac61b6f0ebba71
danypype <danypype@gmail.com>
2014-06-09 18:43:08 -0500

    Handle concurrent requests and aborted requests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var HttpProxy = require("../")
 ,   cachePath = __dirname + "/cache"
-,   cacheTtl = 1000 * 30 //10 minutes
+,   cacheTtl = 1000 * 60 * 10//10 minutes
 ,   sh = require("execSync")
 ,   nock = require("nock")
 ,   request = require("request").defaults({
@@ -136,7 +136,93 @@ describe("HttpProxy", function () {
         done();
       })
     })
-  })
+  });
+
+  describe("Concurrency", function () {
+    before(function () {
+      this.response = "";
+      this.n = 1000;
+
+      for (var i = 0; i < 1000; i++) {
+        this.response += "abcdefgklmopqrstuvwxyz|";
+      };
+
+      nock("http://myserver.com")
+        .filteringPath(/\/.+/, "/x")
+        .get("/x")
+        .times(this.n)
+        .reply(200, this.response);
+
+      nock.disableNetConnect();
+      nock.enableNetConnect("localhost");
+    });
+
+    it("Handles concurrency of new requests", function (done) {
+      var self = this
+      ,   completed = 0;
+
+      for (var i = 0; i < this.n; i++) {
+        request.get("http://myserver.com/" + i, function (error, response, body) {
+          expect(body).to.equal(self.response);
+          if (++completed == self.n) {
+            done();
+          };
+        });
+      };
+    });
+
+    it("Handles concurrency of cached requests", function (done) {
+      var self = this
+      ,   completed = 0;
+
+      for (var i = 0; i < this.n; i++) {
+        request.get("http://myserver.com/" + i, function (error, response, body) {
+          expect(body).to.equal(self.response);
+          if (++completed == self.n) {
+            done();
+          };
+        });
+      };
+    });
+  });
+
+  describe("Aborting requests", function () {
+    before(function () {
+      this.response = "";
+      this.n = 2000;
+
+      for (var i = 0; i < 1000; i++) {
+        this.response += "abcdefgklmopqrstuvwxyz|";
+      };
+
+      nock("http://myserver2.com")
+        .filteringPath(/\/.+/, "/x")
+        .get("/x")
+        .times(this.n)
+        .reply(200, this.response);
+
+      nock.disableNetConnect();
+      nock.enableNetConnect("localhost");
+    });
+
+    it("Handles aborted requests", function (done) {
+      var self = this
+      ,   completed = 0;
+
+      for (var i = 0; i < this.n; i++) {
+        var req = request.get("http://myserver2.com/" + i, function (error, response, body) {
+          expect(body).to.equal(self.response);
+          if (++completed == self.n / 2) {
+            done();
+          };
+        });
+        //Abort 50% of requests
+        if (!(i % 2)) {
+          req.abort();
+        };
+      };
+    });
+  });
 
   after(function () {
     //Remove cache directory
